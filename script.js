0fac0cb49812861028a5314cbabb191a5345fc5b
Edgar Cruz <josuecruz615@gmail.com>
2025-05-08 16:46:44 -0600

    Corrijo script.js para conectar bien con Supabase2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const supabaseUrl = 'https://pmdnbjorfimihsyxmfzh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtZG5iam9yZmltaWhzeXhtZnpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3MzUyMDAsImV4cCI6MjA2MjMxMTIwMH0.SFs7X2GqxCWGaqOShhvykPvgFAba_w0qit2sIXXH-bg.';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtZG5iam9yZmltaWhzeXhtZnpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3MzUyMDAsImV4cCI6MjA2MjMxMTIwMH0.SFs7X2GqxCWGaqOShhvykPvgFAba_w0qit2sIXXH-bg';
 
 
 
