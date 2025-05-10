f7b019ab408d904097fe6960bdcb9269f7fe91f7
Edgar Cruz <josuecruz615@gmail.com>
2025-05-10 16:18:44 -0600

    Primer commit: subida del proyecto ZAPATONGT

---
+++ js/supabase-client.js
@@ -0,0 +1,4 @@
+<!-- /js/supabase-client.js -->
+const supabaseUrl = 'https://pmdnbjorfimihsyxmfzh.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtZG5iam9yZmltaWhzeXhtZnpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3MzUyMDAsImV4cCI6MjA2MjMxMTIwMH0.SFs7X2GqxCWGaqOShhvykPvgFAba_w0qit2sIXXH-bg';
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
